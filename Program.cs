a23f45f25f5532dea85ca3e24c938c5d7ac8aa95
sergiosssg <sergiosssg@gmail.com>
2021-12-20 18:26:44 +0200

    added  function  firebirdDBinfo....

--- Program.cs
+++ Program.cs
@@ -16,33 +16,21 @@ namespace ConsoleAppFbFramework019
     {
         static void Main(string[] args)
         {
-            connToDBsimple();
+            string strConnection2FB = makeCoonectionString2FB();
 
-            connToDBEntity();
+
+
+            firebirdDBinfo( strConnection2FB);
+
+            connToDBsimple(strConnection2FB);
+
+            connToDBEntity(strConnection2FB);
         }
 
 
-        public static DbContext  connToDBEntity()
+        private static DbContext  connToDBEntity(string sConnectionString)
         {
-            string strExePath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
-            DbConnectionStringBuilder dbConnectionStringBuilder = new DbConnectionStringBuilder();
-
-
-            dbConnectionStringBuilder["Data Source"] = "localhost";
-            //dbConnectionStringBuilder["Initial Catalog"] = @"C:\SSG\PROJECTs\TELET\DB4TELEFONE\sampd_cexs.fdb";//"sampd_cexs";
-            dbConnectionStringBuilder["Database"] = Path.Combine(strExePath, "sampd_cexs.fdb");
-
-            dbConnectionStringBuilder["User ID"] = "sysdba";
-            dbConnectionStringBuilder["Password"] = "masterkey";
-
-            dbConnectionStringBuilder["Charset"] = "UTF8";
-
-
-            dbConnectionStringBuilder["Embedded"] = FbServerType.Embedded;
-            //dbConnectionStringBuilder["integrated Security"] = "SSPI";
-            string sCon = dbConnectionStringBuilder.ConnectionString;
-
-            using (var dbContent = new DbAppContext(sCon))
+            using (var dbContent = new DbAppContext(sConnectionString))
             {
 
                 var simpleQueryOfVidConnects = dbContent.pO_TEL_VID_CONNECTs.Where(s => s.Id > 0);
@@ -64,32 +52,13 @@ namespace ConsoleAppFbFramework019
         }
 
 
-        public static int connToDBsimple()
+        private static int connToDBsimple( string sConnectionString)
         {
-
-            string strExePath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
-
-            DbConnectionStringBuilder dbConnectionStringBuilder = new DbConnectionStringBuilder(); //new SqlConnectionStringBuilder();
-
-            dbConnectionStringBuilder["Data Source"] = "localhost";
-            //dbConnectionStringBuilder["Initial Catalog"] = @"C:\SSG\PROJECTs\TELET\DB4TELEFONE\sampd_cexs.fdb";//"sampd_cexs";
-            dbConnectionStringBuilder["Database"] = Path.Combine(strExePath, "sampd_cexs.fdb");
-
-            dbConnectionStringBuilder["User ID"] = "sysdba";
-            dbConnectionStringBuilder["Password"] = "masterkey";
-
-            dbConnectionStringBuilder["Charset"] = "UTF8";
-
-
-            dbConnectionStringBuilder["Embedded"] = FbServerType.Embedded;
-            //dbConnectionStringBuilder["integrated Security"] = "SSPI";
-            string sCon = dbConnectionStringBuilder.ConnectionString;
-
             bool correctConnectionToDB = false;
             int  retCount  = 0;
 
 
-            using (DbConnection dbConnection = new FbConnection(sCon))
+            using (DbConnection dbConnection = new FbConnection( sConnectionString))
             {
                 try
                 {
@@ -121,7 +90,87 @@ namespace ConsoleAppFbFramework019
             return retCount;
         }
 
+        private static void firebirdDBinfo( string strConnection)
+        {
+            using (DbConnection dbConnection = new FbConnection(strConnection))
+            {
+                try
+                {
+                    FbDatabaseInfo dbInfo = new FbDatabaseInfo((FbConnection)dbConnection);
 
+
+
+                    Console.WriteLine("Server Version: {0}", dbInfo.ServerVersion);
+                    Console.WriteLine("ISC Version : {0}", dbInfo.IscVersion);
+                    Console.WriteLine("Server Class : {0}", dbInfo.ServerClass);
+                    Console.WriteLine("Max memory : {0}", dbInfo.MaxMemory);
+                    Console.WriteLine("Current memory : {0}", dbInfo.CurrentMemory);
+                    Console.WriteLine("Page size : {0}", dbInfo.PageSize);
+                    Console.WriteLine("ODS Mayor version : {0}", dbInfo.OdsVersion);
+                    Console.WriteLine("ODS Minor version : {0}", dbInfo.OdsMinorVersion);
+                    Console.WriteLine("Allocation pages: {0}", dbInfo.AllocationPages);
+                    Console.WriteLine("Base level: {0}", dbInfo.BaseLevel);
+                    Console.WriteLine("Database id: {0}", dbInfo.DbId);
+                    Console.WriteLine("Database implementation: {0}", dbInfo.Implementation);
+                    Console.WriteLine("No reserve: {0}", dbInfo.NoReserve);
+                    Console.WriteLine("Forced writes: {0}", dbInfo.ForcedWrites);
+                    Console.WriteLine("Sweep interval: {0}", dbInfo.SweepInterval);
+                    Console.WriteLine("Number of page fetches: {0}", dbInfo.Fetches);
+                    Console.WriteLine("Number of page marks: {0}", dbInfo.Marks);
+                    Console.WriteLine("Number of page reads: {0}", dbInfo.Reads);
+                    Console.WriteLine("Number of page writes: {0}", dbInfo.Writes);
+                    Console.WriteLine("Removals of a version of a record: {0}", dbInfo.BackoutCount);
+                    Console.WriteLine("Number of database deletes: {0}", dbInfo.DeleteCount);
+                    Console.WriteLine("Number of removals of a record and all of its ancestors: {0}", dbInfo.ExpungeCount);
+                    Console.WriteLine("Number of inserts: {0}", dbInfo.InsertCount);
+                    Console.WriteLine("Number of removals of old versions of fully mature records: {0}", dbInfo.PurgeCount);
+                    Console.WriteLine("Number of reads done via an index: {0}", dbInfo.ReadIdxCount);
+                    Console.WriteLine("Number of sequential sequential table scans: {0}", dbInfo.ReadSeqCount);
+                    Console.WriteLine("Number of database updates: {0}", dbInfo.UpdateCount);
+                    Console.WriteLine("Database size in pages: {0}", dbInfo.DatabaseSizeInPages);
+                    Console.WriteLine("Number of the oldest transaction: {0}", dbInfo.OldestTransaction);
+                    Console.WriteLine("Number of the oldest active transaction: {0}", dbInfo.OldestActiveTransaction);
+                    Console.WriteLine("Number of the oldest active snapshot: {0}", dbInfo.OldestActiveSnapshot);
+                    Console.WriteLine("Number of the next transaction: {0}", dbInfo.NextTransaction);
+                    Console.WriteLine("Number of active transactions: {0}", dbInfo.ActiveTransactions);
+
+
+
+                }
+                catch (SqlException sqlEx)
+                {
+
+                }
+            }
+
+
+
+        }
+
+
+        private static string makeCoonectionString2FB()
+        {
+
+            string strExePath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            DbConnectionStringBuilder dbConnectionStringBuilder = new DbConnectionStringBuilder();
+
+
+            dbConnectionStringBuilder["Data Source"] = "localhost";
+            //dbConnectionStringBuilder["Initial Catalog"] = @"C:\SSG\PROJECTs\TELET\DB4TELEFONE\sampd_cexs.fdb";//"sampd_cexs";
+            dbConnectionStringBuilder["Database"] = Path.Combine(strExePath, "sampd_cexs.fdb");
+
+            dbConnectionStringBuilder["User ID"] = "sysdba";
+            dbConnectionStringBuilder["Password"] = "masterkey";
+
+            dbConnectionStringBuilder["Charset"] = "UTF8";
+
+
+            dbConnectionStringBuilder["Embedded"] = FbServerType.Embedded;
+            //dbConnectionStringBuilder["integrated Security"] = "SSPI";
+
+            return dbConnectionStringBuilder.ConnectionString;
+
+        }
 
     }
 }
